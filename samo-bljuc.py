2c9729f126b6047128e39f5c747f48eae1df5f4c
Srđan Pavlović <88070690+srdjanpavlovic@users.noreply.github.com>
2021-07-30 04:02:20 +0200

    Update samo-bljuc.py

--- samo-bljuc.py
+++ samo-bljuc.py
@@ -2,14 +2,22 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
+mesec = 4  # ovde se definise za koji mesec skidamo podatke!!!
+
+dana_u_mesecu = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  # ne dirati ovu listu naravno :)
+
+# moglo je i sa .tolower elegantnije, al ajde nema veze,
+# definisemo istu rec all-caps i sa pocetnim velikim za pretragu posle:
+
 trazena_rec1 = 'Đilas'
 trazena_rec2 = 'ĐILAS'
+
 reci_na_dan = 0
 info_dan = []
 
-for dan in range(1, 29):
+for dan in range(1, int(dana_u_mesecu[mesec-1]) + 1):
     for stranica in range(1, 11):
-        link = (f'https://www.vesti.rs/arhiva/2021/2/{dan}/blic/{stranica}/')
+        link = (f'https://www.vesti.rs/arhiva/2021/{mesec}/{dan}/blic/{stranica}/')
         sadrzaj = BeautifulSoup(requests.get(link).content, "html.parser")
         if str(sadrzaj).count(trazena_rec1) > 0 or str(sadrzaj).count(trazena_rec2) > 0:
             print('Tražena reč detektovana!')
@@ -20,5 +28,5 @@ for dan in range(1, 29):
     print(info_dan)
     reci_na_dan = 0
 
-with open('podaci-blic-februar.txt','w', encoding='utf-8') as f:
+with open(f'podaci-blic-mesec-{mesec}.txt', 'w', encoding='utf-8') as f:
     f.write(str(info_dan))
